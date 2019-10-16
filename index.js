390d595272acb2fde8746df1b4e853d46afbfa53
draqxla <56518037+draqxla@users.noreply.github.com>
2019-10-16 17:16:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,27 +2,6 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const ms = require('ms');
 const ytdl = require("ytdl-core");
-//const fs = require('fs');
-//bot.commands = new Discord.Collection();
-
-// fs.readdir("./commands/", (err, files) => {
-
-//     if(err) console.log(err);
-
-//     let jsfile = files.filter(f => f.split(".").pop() === "js")
-//     if (jsfile.length <= 0) {
-//         console.log("Cound't find commands.");
-//         return;
-//     }
-
-//     jsfile.forEach((f, i) =>{
-//         let props = require(`./commands/${f}`);
-//         console.log(`${f} loaded.`);
-//         bot.commands.set(props.help.name, props);
-//     });
-
-
-// });
 
 const PREFIX = '=';
 
